1c55690c7014bf71c08aa70197ec611accd231bf
Irek <irekwrobel23@gmail.com>
2018-10-18 21:48:46 +0200

    Pobieranie adresu broadcast, tworzenie socketu do nasłuchiwania

--- P2P-Java-desktop/src/main/java/tin/p2p/socketLayer/SocketManager.java
+++ P2P-Java-desktop/src/main/java/tin/p2p/socketLayer/SocketManager.java
@@ -1,5 +1,58 @@
 package tin.p2p.socketLayer;
 
+import java.io.IOException;
+import java.net.*;
+import java.util.Enumeration;
+
 public class SocketManager {
-} 
+    private final static int PORT = 8888;
+
+    private ServerSocket createAndConfigureSocketForListening() {
+        ServerSocket serverSocket = null;
+
+        try {
+            serverSocket = new ServerSocket(PORT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return serverSocket;
+    }
+
+    private InetAddress getBroadcastAddress() {
+        Enumeration<NetworkInterface> interfaces = null;
+
+        try {
+            interfaces = NetworkInterface.getNetworkInterfaces();
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+
+        if (interfaces != null) {
+
+            while (interfaces.hasMoreElements()) {
+                NetworkInterface networkInterface = interfaces.nextElement();
+
+                try {
+                    if (networkInterface.isLoopback()) {
+                        continue;    // Do not want to use the loopback interface.
+                    }
+                } catch (SocketException e) {
+                    e.printStackTrace();
+                }
+
+                for (InterfaceAddress interfaceAddress : networkInterface.getInterfaceAddresses()) {
+                    InetAddress broadcast = interfaceAddress.getBroadcast();
+                    if (broadcast == null) {
+                        continue;
+                    }
+
+                    return broadcast;
+                }
+            }
+        }
+        return InetAddress.getLoopbackAddress();
+    }
+
+}
 
