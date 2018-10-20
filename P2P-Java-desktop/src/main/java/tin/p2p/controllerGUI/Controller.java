8c5c6e522a59db734d652e7c3ce0197e50fdd3cc
Irek <irekwrobel23@gmail.com>
2018-10-20 19:10:56 +0200

    Dodanie listy w widoku

--- P2P-Java-desktop/src/main/java/tin/p2p/controllerGUI/Controller.java
+++ P2P-Java-desktop/src/main/java/tin/p2p/controllerGUI/Controller.java
@@ -1,4 +1,29 @@
 package tin.p2p.controllerGUI;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
 public class Controller {
+    @FXML
+    private Button showOtherNodesBtn;
+
+    @FXML
+    private ListView<String> nodesList;
+
+    public static final ObservableList<String> data = FXCollections.observableArrayList();
+
+    @FXML
+    void handleOtherNodesBtnClick(ActionEvent event) {
+        data.clear();
+        data.addAll("test1", "test2");
+        nodesList.setItems(data);
+        System.out.println(nodesList.getItems().toString());
+
+
+
+    }
 }
